c312417831342deac19e8eb2cc1586bd2713b095
Osama Shahid <osama_o.s@hotmail.com>
2026-04-12 00:34:01 -0400

    implemented: alembic for db migrations

--- apps/api/app/core/config.py
+++ apps/api/app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     open_api_key: str
+    database_url: str
 
 
 settings = Settings()
