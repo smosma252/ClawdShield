9a2148cd291b1a81c1803a1b4fb271b94fb92553
Osama Shahid <osama_o.s@hotmail.com>
2026-04-13 01:59:57 -0400

    implemented: async context manager on startup init

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -1,11 +1,17 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from sqlmodel import Session
+
 from .api.v1.router import router as api_v1_router
-from .db.client import create_db_and_tables
+from .db.client import create_db_and_tables, engine
+from .services.attacks.loader import load_scenarios
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    with Session(engine) as session:
+        load_scenarios(session)
     yield
 
 
