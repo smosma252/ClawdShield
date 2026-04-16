82be0bfff7122f8f87924b21b9a0e92abedade9e
Osama Shahid <osama_o.s@hotmail.com>
2026-04-16 00:58:34 -0400

    implemented: cors for frontend communication

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -1,6 +1,8 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from sqlmodel import Session
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from .api.v1.router import router as api_v1_router
 from .db.client import create_db_and_tables, engine
@@ -14,6 +16,15 @@ async def lifespan(app: FastAPI):
         load_scenarios(session)
     yield
 
+orgins = [
+    "http://localhost",
+    "http://localhost:5173"
+]
 
 app = FastAPI(title="ClawdShield", version="0.1.0", lifespan=lifespan)
+app.add_middleware(CORSMiddleware, 
+                   orgins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
 app.include_router(api_v1_router)
