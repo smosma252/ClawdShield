d6ce6904c963747a79174be88c97c5ad60a76005
Osama Shahid <osama_o.s@hotmail.com>
2026-04-16 00:59:18 -0400

    implemnted: scenario retrieval and creation route.

--- apps/api/app/api/v1/scenario.py
+++ apps/api/app/api/v1/scenario.py
@@ -1,20 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlmodel import Session, select
+from app.db.client import get_session
+from app.db.models import Scenario
+from app.schemas.scenario import CreateScenarioRequest
 
 router = APIRouter(prefix='/scenarios', tags=['scenarios'])
 
-
 @router.get("/")
-async def get_all_scenarios():
-    '''
-    Get all scenarios for attacks
-    '''
-    pass
-
+async def get_all_scenarios(session: Session = Depends(get_session)):
+    scenarios = session.exec(select(Scenario)).all()
+    return scenarios
 
 @router.get("/{id}")
-async def get_scenario(id):
-    '''
-    Get Scenario based on ID
-    '''
-    pass
+async def get_scenario(id, session: Session = Depends(get_session)):
+    scenarios = session.exec(select(Scenario).where(Scenario.id == id)).first()
+    return scenarios
 
+@router.post("/")
+async def create_scenario(scenario: CreateScenarioRequest, session: Session = Depends(get_session)):
+    try:
+        session.add(Scenario(
+            scenario_key=scenario.scenario_key,
+            name=scenario.name,
+            category=scenario.category,
+            severity=scenario.severity,
+            user_task=scenario.user_task,
+            injected_content=scenario.injected_content,
+            expected_safe_actions=scenario.expected_safe_actions
+        ))
+        session.commit()
+    except Exception as e:
+        return {"message": f"Unable to import Scenario: {str(e)}"}
+        
